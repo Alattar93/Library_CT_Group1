04b7293d1c7f2cf2db67fdaa1877925d1f75e7ea
Mohamed Alattar <mnowzad@gmail.com>
2021-02-25 17:44:46 -0800

    ss

---
+++ src/test/java/com/libraryCT/step_definitions/TC10.java
@@ -0,0 +1,35 @@
+package com.libraryCT.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TC10 {
+
+    @Given("the user\\(librarian) on the homepage")
+    public void the_user_librarian_on_the_homepage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+    @When("the user click users module")
+    public void the_user_click_users_module() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user should see the following column names:")
+    public void the_user_should_see_the_following_column_names(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ target/test-classes/features/TC-3-SeeModucules.feature
@@ -0,0 +1,18 @@
+@all
+Feature: User able to see several modules
+  As a user ,I should able to see several modules
+
+    Scenario: Student should have access to modules
+      Given user is on the login page
+      Given the "student" on the home page
+      Then the user should see following modules
+      |Books|
+      |Borrowing Books|
+
+  Scenario: Student should have access to modules
+    Given user is on the login page
+    Given the "librarian" on the home page
+    Then the user should see following modules
+      |Dashboard|
+      |Users   |
+      |Books|
